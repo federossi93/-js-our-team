56ad5f7955d9dd5cc8d1ad967e092b85671be67d
federossi93 <rossifederico0993@gmail.com>
2022-10-20 13:42:08 +0200

    intial commit

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+//MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+//MILESTONE 1: Stampare su console le informazioni di nome, ruolo e la stringa della foto
+//MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
